d659e4fe06b2adb4ac8d550a534e01facc186bc2
Patrick Arlt <patrick.arlt@gmail.com>
2016-08-15 11:25:22 -0700

    final inline notes

--- src/codemirror-editor/codemirror-editor.component.ts
+++ src/codemirror-editor/codemirror-editor.component.ts
@@ -3,21 +3,43 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import * as CodeMirror from 'codemirror';
 import 'codemirror/mode/gfm/gfm';
 import 'codemirror/addon/edit/continuelist';
-import 'codemirror/addon/hint/show-hint';
-import 'codemirror/addon/hint/anyword-hint';
 
 const noop = () => {};
 
+/**
+ * <codemirror-editor> is a wrapper around CodeMirror that integrates it with
+ * Angular 2 as a custom form control. It implements `ControlValueAccessor` so
+ * it can be used with `ngModel` in `<note-editor>`. The process of hooking this
+ * up is fairly simple but requires a deep conceptual understanding of how a lot
+ * of stuff works in Angular 2.0. There were several articles that were
+ * particularly helpful for me in figuring this out. Once you do this it becomes
+ * trivial to use `ngModel` or to have a component represent a form control.
+ *
+ * http://blog.thoughtram.io/angular/2016/07/27/custom-form-controls-in-angular-2.html
+ * http://almerosteyn.com/2016/04/linkup-custom-control-to-ngcontrol-ngmodel
+ */
 @Component({
   selector: 'codemirror-editor',
   template: require('./codemirror-editor.component.html'),
   styles: [
     require('codemirror/lib/codemirror.css'),
-    require('codemirror/addon/hint/show-hint.css'),
     require('./codemirror-editor.component.scss'),
   ],
+  /**
+   * Since we are including the CodeMirror css as well as our custom styles we
+   * need to tell Angular that it should not encapsulate this CSS.
+   */
   encapsulation: ViewEncapsulation.None,
   providers: [
+    /**
+     * This is a little magic supplied by several articles. Exactly what it does
+     * I am not entirely sure of. The dependency injection article does go into
+     * more detail about using `forwardRef` though.
+     *
+     * http://blog.thoughtram.io/angular/2015/05/18/dependency-injection-in-angular-2.html
+     * http://blog.thoughtram.io/angular/2016/07/27/custom-form-controls-in-angular-2.html
+     * http://almerosteyn.com/2016/04/linkup-custom-control-to-ngcontrol-ngmodel
+     */
     {
       provide: NG_VALUE_ACCESSOR,
       useExisting: forwardRef(() => CodeMirrorEditorComponent),
@@ -25,9 +47,13 @@ const noop = () => {};
     }
   ]
 })
-export class CodeMirrorEditorComponent implements ControlValueAccessor {
+export class CodeMirrorEditorComponent implements ControlValueAccessor, AfterViewInit {
   private cm :any;
-  private timeout;
+
+  /**
+   * Like the list of tags in <tag-input> we use a getter/setter pair to make it
+   * easier to inform update the value of the internal CodeMirror editor.
+   */
   private _content: string = '';
 
   get content () {
@@ -37,16 +63,33 @@ export class CodeMirrorEditorComponent implements ControlValueAccessor {
   set content (value: any) {
     this._content = value || '';
 
+    /**
+     * Note that we make sure we have CodeMirror setup before we try to set the
+     * value since this might be called before the view initalizes and we setup
+     * CodeMirror.
+     */
     if (this.cm) {
       this.cm.getDoc().setValue(this.content);
     }
   }
 
+  /**
+   * We will need a reference to an actual DOM element to hook up to CodeMirror
+   * so we can get a reference to the element with `#editor` here.
+   */
   @ViewChild('editor') editor :ElementRef;
 
   constructor () {}
 
+  /**
+   * Now we can implement AfterViewInit which is fired after Angular sets up the
+   * view for the first time.
+   */
   ngAfterViewInit () {
+    /**
+     * This initalizes CodeMirror with our editor element and a few small extras
+     * like a GitHub Flavored Markdown, list completion, and infinite scroll.
+     */
     this.cm = CodeMirror.fromTextArea(this.editor.nativeElement, {
       viewportMargin: Infinity,
       lineWrapping: true,
@@ -56,50 +99,50 @@ export class CodeMirrorEditorComponent implements ControlValueAccessor {
       }
     });
 
+    /**
+     * Now that we have a CodeMirror instance we can set its value to show the
+     * proper content.
+     */
     this.cm.getDoc().setValue(this.content);
 
+    /**
+     * Whenever CodeMirror gives us a changes event we can pass that to the
+     * `ControlValueAccessor` callback to inform Angular that the value of this
+     * input has changed.
+     */
     this.cm.on('changes', () => {
       this.onChangeCallback(this.cm.getValue());
     });
-
-    this.cm.on("inputRead", (cm) => {
-      if (this.timeout) {
-        clearTimeout(this.timeout)
-      };
-
-      this.timeout = setTimeout(() => {
-        CodeMirror.showHint(cm, CodeMirror.hint.anyword, {
-          completeSingle: false
-        });
-      }, 50);
-    });
-
-    this.cm.on("keyup", (cm, event) => {
-      var keyCode = event.keyCode || event.which;
-      if (keyCode == 8 || keyCode == 46) {
-        if(this.timeout) {
-          clearTimeout(this.timeout);
-        }
-
-        this.timeout = setTimeout(() => {
-          CodeMirror.showHint(cm, CodeMirror.hint.anyword, {
-            completeSingle: false
-          });
-        }, 50);
-      }
-    });
   }
 
+  /**
+   * Part of the implimentation of `ControlValueAccessor`. `writeValue`
+   * is called whenever the value is updated from an ourside source. It might be
+   * `undefined` so we check manually for that case.
+   */
   writeValue(value: any) {
-    this.content = value || '';
+    if(value !== undefined) {
+      this.content = value;
+    }
   }
 
-  private onTouchedCallback: () => void = noop;
+  /**
+   * Part of the implimentation of `ControlValueAccessor`. `registerOnChange`
+   * gets passed a function that we need to call whenever we change the value
+   * of our input.
+   */
   private onChangeCallback: (_: any) => void = noop;
 
   registerOnChange(fn) {
     this.onChangeCallback = fn;
   }
+
+  /**
+   * Part of the implimentation of `ControlValueAccessor`. `registerOnTouched`
+   * gets passed a function that we need to call whenever the user "touches" or
+   * focuses and unfocuses this input.
+   */
+  private onTouchedCallback: () => void = noop;
 
   registerOnTouched(fn) {
     this.onTouchedCallback = fn;
--- src/note-editor/note-editor.component.ts
+++ src/note-editor/note-editor.component.ts
@@ -1,18 +1,22 @@
 import { Component, ViewChild, EventEmitter, Input, Output } from '@angular/core';
 import { Note } from '../shared/note';
-import { FORM_DIRECTIVES, REACTIVE_FORM_DIRECTIVES, FormGroup, FormControl } from '@angular/forms';
+import { FORM_DIRECTIVES, FormGroup, FormControl } from '@angular/forms';
 import { ViewEncapsulation } from '@angular/core';
 import { CodeMirrorEditorComponent } from '../codemirror-editor/codemirror-editor.component';
 import { TagInputComponent } from '../tag-input/tag-input.component';
-import { NotesDatabaseService } from '../shared/notes-db.service'
 import { NotesStore } from '../shared/notes-store.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { NotePreviewComponent } from '../note-preview/note-preview.component';
 import { RoundPipe } from '../shared/round.pipe';
+import { ScrollTopDirective } from '../shared/scrollTop.directive';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/combineLatest';
-import { ScrollTopDirective } from '../shared/scrollTop.directive';
 
+/**
+ * <note-editor> is the most complicated component in our application. It makes
+ * heavy use of Observables. <note-editor> also combines many built-in and
+ * custom directives which adds to its compledity.
+ */
 @Component({
   selector: 'note-editor',
   template: require('./note-editor.component.html'),
@@ -24,12 +28,16 @@ import { ScrollTopDirective } from '../shared/scrollTop.directive';
     TagInputComponent,
     NotePreviewComponent,
     FORM_DIRECTIVES,
-    REACTIVE_FORM_DIRECTIVES,
     ScrollTopDirective
   ],
-  pipes: [RoundPipe]
+  pipes: [ RoundPipe ]
 })
 export class NoteEditorComponent {
+  /**
+   * since we want to bind our child <codemirror-editor> and <tag-input>
+   * components to `note.content` and `note.tags` respectivly we need an empty
+   * note for them to bind against until we set it up properly.
+   */
   private note: Note = {
     content: '',
     tags: [],
@@ -37,22 +45,46 @@ export class NoteEditorComponent {
     created: new Date(),
     title: ''
   };
+
+  /**
+   * Set a number of private variables we need for later.
+   */
   private loading: boolean;
   private scrollPercentage: number = 0;
   private editorSubscription: any;
   private paramsSubscription: any;
   private routerSubsciption: any;
 
+  /**
+   * Since we want to subscribe to changes in our form to do auto saving we need
+   * a reference to the `#noteForm` that we setup in our template.
+   */
   @ViewChild('noteForm') noteForm;
 
+  /**
+   * We need a few things from the dependency injector here like the router, the
+   * active route and our note store.
+   */
   constructor (
     private router: Router,
     private route: ActivatedRoute,
-    private notesDatabase: NotesDatabaseService,
     private notesStore: NotesStore) {}
 
   ngOnInit() {
+    /**
+     * Now that we have instances of what we need from the dependency injector
+     * we can subscribe to changes in the active route and get the ID of the
+     * currently selected note.
+     *
+     * https://angular.io/docs/ts/latest/guide/router.html#!#route-parameters
+     */
     this.routerSubsciption = this.route.params.subscribe((params: any) => {
+      /**
+       * When the route changes lets make sure the note is loaded into the
+       * noteStore. If we find a note we can reset the scrollPercentage we can
+       * be sure we are at the top of our new note. If we cant find the note
+       * redirect back to the note list.
+       */
       this.notesStore.loadNote(params.id)
         .then(() => {
           this.scrollPercentage = 0;
@@ -64,25 +96,59 @@ export class NoteEditorComponent {
         });
     });
 
+    /**
+     * Now we want to setup our editor to always show the latest note whenever
+     * the note OR the route param changes. Using `Observable.combineLatest` we
+     * can create a new Observable that represents a new value for `note` based
+     * on the latest information.
+     *
+     * http://reactivex.io/rxjs/class/es6/Observable.js~Observable.html#static-method-combineLatest
+     */
     this.paramsSubscription = Observable.combineLatest(
+      /**
+       * Pass in any number of Observables...
+       */
       this.route.params,
       this.notesStore.notes,
+
+      /**
+       * and a function that combines them to return a new value...
+       */
       (params :any, notes) => {
         return notes.find((note) => {
           return note._id === params.id;
         });
       })
+
+      /**
+       * then subscribe to the results of our new Observable.
+       */
       .subscribe((note) => {
+        /**
+         * If we don't have a note we know we are loading one since we load the
+         * note as part of our `paramsSubscription`.
+         */
         if (!note) {
           this.loading = true;
           return;
         }
 
+        /**
+         * We have an updated value for note so lets set it and set loading to
+         * false to show the editor.
+         */
         this.note = note;
         this.loading = false;
       });
   }
 
+  /**
+   * We still want auto-saving so we can subscribe to 'valueChanges' on our
+   * `#noteForm` and add a small debounce and filtering function to reduce noise
+   * and notw auto-save to often.
+   *
+   * https://angular.io/docs/ts/latest/guide/server-communication.html#!#more-observables
+   */
   ngAfterViewInit() {
     this.editorSubscription = this.noteForm.control.valueChanges
       .debounceTime(1000)
@@ -93,17 +159,30 @@ export class NoteEditorComponent {
       });
   }
 
+  /**
+   * Like event handlers we should always unsubscribe from any subscritions we
+   * create to avoid memory leaks.
+   */
   ngOnDestroy () {
     this.paramsSubscription.unsubscribe();
     this.editorSubscription.unsubscribe();
     this.routerSubsciption.unsubscribe();
   }
 
+  /**
+   * Simple function that calculates the percentace we have scrolled in our
+   * editor div. We use this with the [scrollTop] directive to sync scrollbar
+   * position.
+   */
   onScroll (e) {
     let el = e.target;
     this.scrollPercentage = el.scrollTop / (el.scrollHeight - el.clientHeight);
   }
 
+  /**
+   * This is a simple function to delete a note from our store and redirect to
+   * the main notes list.
+   */
   onDelete () {
     this.notesStore.deleteNote(this.note).then((deleted) => {
       this.router.navigate(['/notes']);
